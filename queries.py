3753d8831f1486a1735cdb01525e60715b82f397
Mannomatico <maximilianzaun@gmail.com>
2023-07-03 13:35:05 +0200

    fixed some SQL code

--- queries.py
+++ queries.py
@@ -7,7 +7,7 @@ QUERIES = {
 
     "predict_best_team": f'WITH predictions AS ( \
                         SELECT predicted_TotalWorkingHours, TeamId \
-                        FROM ML.PREDICT(MODEL `vlba-maintenance-and-service.MwwMs_Data.model1`, \
+                        FROM ML.PREDICT(MODEL `{dataset_id}.model1`, \
                         (SELECT \
                         "Electronics repair" as TaskType, \
                         TeamId, \
@@ -25,7 +25,7 @@ QUERIES = {
                         DistanceSP1ToClient, \
                         Quantity, \
                         ServiceTaskId \
-                        FROM `vlba-maintenance-and-service.MwwMs_Data.Team_Facts` \
+                        FROM `{dataset_id}.Team_Facts` \
                         ) \
                         )) \
                         ) \
